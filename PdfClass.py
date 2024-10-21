92eab2bfe7f2fb2cf41dd219986601c6db5947e1
thioaana <i.anastasiadis@ergohellas.gr>
2024-10-21 22:40:53 +0300

    Good job for fist day

--- PdfClass.py
+++ PdfClass.py
@@ -1,19 +1,46 @@
 import PyPDF2
 
 class PdfClass :
-    _phrasesToSearch = ["ΚΑΠΝΟΣ"]
+    '''
+    This class handles the text of a pdf and extracts conclusions
+    '''
+
+    _conclusion = {}    # Will be in the form {contract: _phrasesToSearch}
+
 
     def __init__(self, fn) :
         self._pdfFileName = fn      # Name of the pdf file
         self._text = []             # List of lists. Each list is a page. Each element is a row of the page
         self._pdfReader = None      # The reader object 
+        self._phrasesToSearch = {"ΚΑΠΝΟΣ": [],
+                                "ΟΡΟΣ ΠΡΟΝΟΙΑΣ": [],
+                                "ΑΥΤΟΜΑΤΗ ΑΝΑΣΥΣΤΑΣΗ": [],
+                                "ΝΕΟΑΠΟΚΤΗΘΕΝΤΑ": [],
+                                "ΟΡΟΣ 72": [],
+                                "ΚΑΤΑ ΤΗΝ ΔΙΑΡΚΕΙΑ ΕΡΓΑΣΙΩΝ": [],
+                                "ΖΗΜΙΕΣ ΚΛΕΠΤΗ ΣΤΟ ΚΤΙΡΙΟ": [],
+                                "ΑΞΙΑ ΚΑΙΝΟΥΡΓΙΟΥ": []}
 
-        
+        self._run()
+
+    def _run(self):
+        '''
+        The logic of the project for a specific pdf file
+        '''
+        self._readPdfExtractText()
+        self._searchForPhrases()
+        self._fillCoclusion()
+
+
+
     def _readPdfExtractText(self):
+        '''
+        This fuction takes a pdf file name and extracts the conent of the file in a list of lists 
+        '''
         # Open the file as Read Binary
         pdfFileObj = open(self._pdfFileName, 'rb')
 
-        # Create a pdf reader object.
+        # Create a p000df reader object.
         self._pdfReader = PyPDF2.PdfReader(pdfFileObj)
 
         numPages = len(self._pdfReader.pages)
@@ -24,22 +51,32 @@ class PdfClass :
             self._text.append(pageObj.extract_text().split("\n"))
 
 
-    def searchForPhrases(self):
-        print(self._text[1])
-        for page in self._text[1:] :
-            # print(page)
-            for row in range(len(page)) :
-                print(page[row])
-                break
-            break
-                # print(type(row), " - ", row)
-                # break
-                # if row.find("Φ") :
-                #     print(row)
-                #     break
-        
+    def _searchForPhrases(self):
+        '''
+        This function searches in the text list for spacific phrases included in _phrasesForSearch.
+        Saves the reasult in the _phrasesForSearch dict.
+        '''
+        for key in self._phrasesToSearch :
+            for idp, page in enumerate(self._text[:5]) :
+                for idr, row in enumerate(page) :
+                    if row.find(key) >= 0:
+                        self._phrasesToSearch[key].append(f"{idr}/{idp}-->{row}")
+                        # print(f"{idr}/{idp}-->{row}")
+        # print(self._phrasesToSearch)
 
+
+    def _fillCoclusion(self):
+        def _getContractNum() :
+            for r in self._text[0]:
+                if r.find("ΑΡΙΘΜΟΣ ΑΣΦΑΛΙΣΤΗΡΙΟΥ") >= 0 :
+                    pv = r.split(' ')[3]
+                    return (pv.split('/')[0], pv.split('/')[1])
+        
+        (c, v) = _getContractNum()
+        self._conclusion[c] = {"Version": v, "Justification": self._phrasesToSearch}
+        
     
+
     def getNumOfPages(self):
         # Print the number of pages of the file
         return len(self._text)
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # Install PyPDF2  - pip install PyPDF2
 
-import PyPDF2
+import os
 from PdfClass import PdfClass
 
 if __name__ == "__main__":
     # Read pdf file name
     # As a start the file name is imported directy to a variable
-    fn = "2045883137-14.pdf"
-    contract = PdfClass(fn)
-    contract._readPdfExtractText()
+    # fn = "2045883137-14.pdf"
+    # contract = PdfClass(fn)
 
-    contract.searchForPhrases()
+    # Define the relative folder path
+    relative_folder_path = './pdfFiles'
+
+    # Iterate through the folder
+    for file_name in os.listdir(relative_folder_path):
+        file_path = os.path.join(relative_folder_path, file_name)
+        
+        # Check if it's a file (not a directory)
+        if os.path.isfile(file_path):
+            print(file_name)
+            contract = PdfClass(file_path)
+            
+    print(PdfClass._conclusion)
+
+
 
 
   
